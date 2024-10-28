5ca7a5204c86b05ed7370b8d05b51e585b7dac48
Sowmiya3103 <sowmiyaraj3103@gmail.com>
2024-10-28 15:57:06 +0530

    Account.java

---
+++ Account.java
@@ -0,0 +1,19 @@
+class account
+{
+int accno;
+string name;
+string acctype;
+long balance;
+{
+public static void main(String ar[])
+{
+open account()
+{
+System.out.println("enter account no:"+accno);
+System.out.println("enter name:"+name);
+System.out.println("enter acctype:"+acctype);
+System.out.println("enter balance:"+balace)"
+}
+}
+}
+}
